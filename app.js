ed2a764a0f34bcea21889aa2fc699edbd7e90675
Vincent D'Ippolito <v.anthony.dippolito@gmail.com>
2019-10-19 04:57:24 -0400

    Fixed bad error messages, links still slightly broken

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var dashboardRouter = require('./routes/dashboard');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
@@ -34,20 +34,23 @@ app.use('/dashboard', dashboardRouter);
 
 // routes for specific pages
 app.get('/learning', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/pages', 'learning.html'));
+  res.sendFile(path.join(__dirname, 'public/pages', 'learning.html'));
 });
 app.get('/services', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/pages', 'services.html'));
+  res.sendFile(path.join(__dirname, 'public/pages', 'services.html'));
 });
 
+app.get('/about/why', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'public/pages', 'about/why.html'));
+});
 app.get('/about/faqs', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/pages', 'about/faqs.html'));
+  res.sendFile(path.join(__dirname, 'public/pages', 'about/faqs.html'));
 });
 app.get('/about/contact', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/pages', 'about/contact.html'));
+  res.sendFile(path.join(__dirname, 'public/pages', 'about/contact.html'));
 });
 app.get('/about/testimonials', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/pages', 'about/testimonials.html'));
+  res.sendFile(path.join(__dirname, 'public/pages', 'about/testimonials.html'));
 });
 
 // catch 404 and forward to error handler
