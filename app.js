4b6f06c50529eae155611e50bfe38e027f05893d
Vincent D'Ippolito <v.anthony.dippolito@gmail.com>
2019-10-19 04:24:37 -0400

    Favicon addition

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser'); // for html submit forms
 var logger = require('morgan');
@@ -15,6 +16,7 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'pug');
 
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
