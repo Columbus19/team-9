113ce151852ef7c9ce2774c1ebce9a4839f658ff
Vincent D'Ippolito <v.anthony.dippolito@gmail.com>
2019-10-19 00:44:54 -0400

    Previously git ignored public files

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
   extended: true
